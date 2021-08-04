be4850d906c79d4b0dc50a105941aacaf1ce6aa2
HugoFabianGomez <hfg.hugo@hotmail.com>
2021-08-03 22:26:04 -0300

    Con Personas.cs

---
+++ Dominio/Personas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Personas
+    {
+        public int Idpersona { get; set; }
+        public string Dni{ get; set; }
+        public string ApellidoNombre { get; set; }
+        public string Usuario { get; set; }
+        public DateTime Fecha_Nac { get; set; }
+        public string Mail { get; set; }
+    }
+}
