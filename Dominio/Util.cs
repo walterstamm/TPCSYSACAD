18ae3e13003c7b7ff4a247d93b6fb4c1537daf31
walterstamm <walterstamm92@gmail.com>
2021-10-11 16:08:50 -0300

    cambios

---
+++ Dominio/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public static class Util
+    {
+        public static string usuarioLogeado { get; set; }
+        public static int tipoUsuario { get; set; }
+    }
+}
