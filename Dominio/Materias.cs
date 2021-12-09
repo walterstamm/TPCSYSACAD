6cc3d79ff5182e00d36146e694efce14cdd41352
HugoFabianGomez <hfg.hugo@hotmail.com>
2021-12-09 15:49:50 -0300

    con eliminar y cargar materia

--- Dominio/Materias.cs
+++ Dominio/Materias.cs
@@ -8,18 +8,18 @@ namespace Dominio
 {
     public class Materias
     {
-		public string id_materia { set; get; }
+		public int idmateria { set; get; }
 
-		public string nombre_materia { set; get; } // varchar(50)
+		public string nombremateria { set; get; } // varchar(50)
 
-		public int carga_horaria_minima { set; get; } // INT NOT NULL CHECK(CARGA_HORARIA_MINIMA > 0),
+		//public int carga_horaria_minima { set; get; } // INT NOT NULL CHECK(CARGA_HORARIA_MINIMA > 0),
 
-		public string carrera { set; get; } // VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES CARRERAS(ID_CARRERA),
-		public int año_curso { set; get; } // INT NOT NULL CHECK(AÑO_CURSO = 1 OR AÑO_CURSO = 2),
+		public int idcarrera { set; get; } // VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES CARRERAS(ID_CARRERA),
+		public byte anio { set; get; } // INT NOT NULL CHECK(AÑO_CURSO = 1 OR AÑO_CURSO = 2),
 
-		public int cuatrimestre { set; get; } // INT NOT NULL CHECK(CUARTRIMESTRE = 1 OR CUARTRIMESTRE = 2),
+		public byte cuatrimestre { set; get; } // INT NOT NULL CHECK(CUARTRIMESTRE = 1 OR CUARTRIMESTRE = 2),
 
-		public bool estado { set; get; } //	 BIT NOT NULL DEFAULT(1)
+		public string estado { set; get; } //	 BIT NOT NULL DEFAULT(1)
 
 	}
 }
